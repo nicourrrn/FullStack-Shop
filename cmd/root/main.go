a838d2d4f126262619c334680ef0d8868d98a2d3
Vadym Sorokin <nicourrrn@gmail.com>
2021-12-24 01:26:30 +0200

    Add my logger

--- cmd/root/main.go
+++ cmd/root/main.go
@@ -3,10 +3,23 @@ package main
 import (
 	"fmt"
 	orderModel "fullstack-shop/pkg/kernel/model/obj/order"
+	"github.com/nicourrrn/littleLogger"
+	"log"
+	"os"
 )
 
 func main() {
+	logFile, _ := os.Create("logs/log.txt")
+	logger, err := littleLogger.NewLogger(logFile, 1)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	logger.Info("Hello")
+	logger.SetFormatter(func() string {
+		return "$msg\n"
+	})
 	order := orderModel.NewOrder(10)
+	logger.Debug("New order make")
 	order.AddProduct(1)
 	order.AddProduct(2)
 	order.Cell(true)
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module fullstack-shop
 
 go 1.17
+
+require github.com/nicourrrn/littleLogger v1.0.0
